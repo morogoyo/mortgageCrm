01fff334ca0b6b168cc0b7bd902dcd8b53395c08
Noble Digital Services <morogoyo@gmail.com>
2023-07-07 11:56:53 -0400

    delete logic needs support from back end to recieve a list WIP

--- src/app/leads/view-leads/view-leads.component.ts
+++ src/app/leads/view-leads/view-leads.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CrudService} from "../../services/leads/crud.service";
 import {Leads} from "../../_shared/interfaces/leads";
+import {DataTable} from "simple-datatables";
 
 @Component({
   selector: 'app-view-leads',
@@ -10,24 +11,35 @@ import {Leads} from "../../_shared/interfaces/leads";
 export class ViewLeadsComponent implements OnInit {
 
 
-
   constructor(private crudService: CrudService) {
   }
 
   leads: any;
 
-  ngOnInit(): void {
+  leadsToRemove: Leads;
 
+  ngOnInit(): void {
+    // const dataTable = new DataTable("#leadsTable");
     this.getAllLeads()
 
   }
 
   getAllLeads() {
-     this.crudService.viewAllLeads().subscribe((data) => {
+    this.crudService.viewAllLeads().subscribe((data) => {
       console.log('this is the viewAllLeads() that is giving me shit ');
       console.log(data);
       // console.log(data.leads.fname)
       this.leads = data;
     });
   }
+
+  updateLeadsToRemove(){
+    this.leadsToRemove =
+  }
+
+  deleteLeads(leads: Leads){
+    leadsToRemove
+  }
 }
+
+
